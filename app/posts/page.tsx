51ec8c86f7283ef19f16b43cf940d6a2cb6fe809
Tran Bich Tuyen <tuyentr1204@gmail.com>
2025-12-01 10:03:44 +0700

    Update Prisma client import and generator config

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import type { Prisma } from "@prisma/client";
 
 export default async function Posts() {
   const posts = await prisma.blog.findMany({
@@ -7,13 +8,16 @@ export default async function Posts() {
     },
   });
 
+  type PostWithUser = Prisma.BlogGetPayload<{
+    include: { user: true };
+  }>;
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16 text-[#333333]">
-      <h1 className="text-4xl font-bold mb-8 ">
-        Posts
-      </h1>
-      <ul className=" max-w-2xl space-y-4">
-        {posts.map((post) => (
+      <h1 className="text-4xl font-bold mb-8">Posts</h1>
+
+      <ul className="max-w-2xl space-y-4">
+        {posts.map((post: PostWithUser) => (
           <li key={post.id}>
             <span className="font-semibold">{post.title}</span>
             <span className="text-sm text-gray-600 ml-2">
